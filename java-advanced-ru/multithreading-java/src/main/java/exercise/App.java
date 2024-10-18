1c6422035d1fab44195d1d3ad969002eb9fc7baa
@hexlet/cli <support@hexlet.io>
2024-10-18 21:06:49 +0500

    submit java-advanced-ru/multithreading-java

---
+++ java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
@@ -0,0 +1,40 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+class App {
+    private static final Logger LOGGER = Logger.getLogger("AppLogger");
+
+    // BEGIN
+    public static Map<String, Integer> getMinMax(int[] numbers) {
+        MaxThread threadMax = new MaxThread(numbers);
+        MinThread threadMin = new MinThread(numbers);
+
+        Map<String, Integer> result = new HashMap<>();
+
+        threadMax.start();
+        LOGGER.info("INFO: Thread " + threadMax.getName() + " started");
+
+        threadMin.start();
+        LOGGER.info("INFO: Thread " + threadMin.getName() + " started");
+
+        try {
+            threadMin.join();
+            LOGGER.log(Level.INFO, "Thread " + threadMax.getName() + " finished");
+
+            threadMax.join();
+            LOGGER.log(Level.INFO, "Thread " + threadMin.getName() + " finished");
+
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted");
+        }
+
+        result.put("min", threadMin.getMinNumber());
+        result.put("max", threadMax.getMaxNumber());
+        return result;
+    }
+    // END
+}
